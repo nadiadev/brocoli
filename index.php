0462bbd87c5e8c4c8d5142fa788d926df347b1e5
nadiadev <creation.nadia@gmail.com>
2015-07-17 10:52:14 +0200

    Add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Brocoli</title>
 </head>
 <body>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, ut, deleniti, 
+		quas vitae ipsa omnis voluptatem totam neque doloremque ad at praesentium 
+		laudantium molestias similique eveniet laborum in quisquam incidunt.</p>
 	
 </body>
 </html>
